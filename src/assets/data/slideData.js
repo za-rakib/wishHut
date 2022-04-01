0f3e415aa11255c7ffffd2abc5fffa3e162d1890
Rakib <phpzarakib@gmail.com>
2022-04-02 01:30:44 +0600

    Slider

---
+++ src/assets/data/slideData.js
@@ -0,0 +1,26 @@
+import slide1 from "../slidePic4-remove.png";
+import slide2 from "../slidepic2.jpg";
+import slide3 from "../slidepic3.jpg";
+export const slideItems = [
+  {
+    id: 1,
+    img: `${slide1}`,
+    title: "SUMMER SALE",
+    des: "DON'T COMPARES ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVAL",
+    bg: "fcf0ff",
+  },
+  {
+    id: 2,
+    img: `${slide2}`,
+    title: "WINTER SALE",
+    des: "DON'T COMPARES ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVAL",
+    bg: "f0f0ff",
+  },
+  {
+    id: 3,
+    img:`${slide3}`,
+    title: "POPULAR SALE",
+    des: "DON'T COMPARES ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVAL",
+    bg: "edffef",
+  },
+];
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,33 +1,65 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Slider.module.css";
-import slide4 from "../../assets/slidePic4-remove.png";
+import { slideItems } from "../../assets/data/slideData";
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+`;
 const Arrow = styled.div`
   left: ${(props) => props.direction === "left" && "10px"};
   right: ${(props) => props.direction === "right" && "10px"};
 `;
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #${(props) => props.bg};
+`;
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
+  console.log("slideItems", slideItems);
   return (
     <div className={classes.sliderContainer}>
-      <Arrow direction="left" className={classes.sliderArrow}>
+      <Arrow
+        direction="left"
+        className={classes.sliderArrow}
+        onClick={() => handleClick("left")}
+      >
         <ArrowLeftOutlined />
       </Arrow>
-      <div className={classes.slideWrapper}>
-        <div className={classes.slides}>
-          <div className={classes.imgContainer}>
-            <img src={slide4} alt="pic" />
-          </div>
-          <div className={classes.infoContainer}>
-            <h1>SUMMER SAL</h1>
-            <p>DON'T COMPARES ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVAL</p>
-            <button>SHOW NOW</button>
-          </div>
-        </div>
-      </div>
-      <Arrow direction="right" className={classes.sliderArrow}>
+      <Wrapper slideIndex={slideIndex}>
+        {slideItems.map((item) => (
+          <Slide bg={item.bg}>
+            <div className={classes.imgContainer}>
+              <img src={item.img} alt="pic" />
+            </div>
+            <div className={classes.infoContainer}>
+              <h1>{item.title}</h1>
+              <p>{item.des}</p>
+
+              <button>SHOW NOW</button>
+            </div>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow
+        direction="right"
+        className={classes.sliderArrow}
+        onClick={() => handleClick("right")}
+      >
         <ArrowRightOutlined />
       </Arrow>
     </div>
